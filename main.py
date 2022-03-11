532ccb13a1bb736f790ff25486af9d6c22079a66
Zachary-zk <xs.zachary.zk@gmail.com>
2022-03-12 01:49:45 +0800

    :sparkles: 并发下载视频

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# 这是一个示例 Python 脚本。
-
-# 按 ⌃R 执行或将其替换为您的代码。
-# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
+import json
+import re
+import requests
+from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
 
 
-# 按间距中的绿色按钮以运行脚本。
+def get_response(html_url, stream=False):
+    headers = {
+        "referer": "https://www.bilibili.com/video/BV1dS4y167y5?from=search&seid=4946859608231270468&spm_id_from=333.337.0.0",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+    }
+    response = requests.get(html_url, headers=headers, stream=stream)
+    return response
+
+
+def get_url(html_url):
+    r = get_response(html_url)
+    a = re.search(r"window\.__INITIAL_STATE__=(.*?);", r.text).group(1)
+    json_data = json.loads(a, strict=False)
+    video_data = json_data['videoData']
+    avid = str(video_data['aid'])
+    qn = '112'
+    urls = []
+
+    for p in video_data['pages']:
+        cid = str(p['cid'])
+        title = p['part']
+        if len(video_data['pages']) == 1:
+            title = video_data['title']
+        urls.append({
+            'title': title,
+            "url": "https://api.bilibili.com/x/player/playurl?cid=" + cid + "&avid=" + avid + "&an=" + qn + "&otype=json&fourk=1"
+        })
+    return urls
+
+
+def save(video_url, video_size, title):
+    video_res = get_response(video_url, True)
+    with open(title + '.mp4', 'wb') as fd:
+        print('开始下载文件：{}, 当前文件大小：{}KB'.format(title, video_size / 1024))
+        for c in tqdm(iterable=video_res.iter_content(), total=video_size, unit='k', desc=None):
+            fd.write(c)
+        print('{},文件下载成功'.format(title))
+
+
+def a_single_download(info):
+    response = get_response(info['url'])
+    json_data = json.loads(response.text)
+    video_url = json_data['data']['durl'][0]['url']
+    video_size = json_data['data']['durl'][0]['size']
+    save(video_url, video_size, info['title'])
+
+
+def concurrent_download(base_infos):
+    executor = ThreadPoolExecutor(max_workers=10)
+    futur_tasks = [executor.submit(a_single_download(), info) for info in base_infos]
+    wait(futur_tasks, return_when=ALL_COMPLETED)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    html_url = "https://www.bilibili.com/video/BV1dS4y167y5?from=search&seid=4946859608231270468&spm_id_from=333.337.0.0"
+    base_infos = get_url(html_url)
+    a_single_download(base_infos[0])
